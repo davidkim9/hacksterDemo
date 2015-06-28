cd373acb97de64a001f0f267c5b4d6c66c316b6e
davidkim9 <davidkim9@gmail.com>
2015-06-27 23:48:41 -0400

    Added test mongo code

--- server.js
+++ server.js
@@ -10,3 +10,21 @@ var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
 });
+
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.CUSTOMCONNSTR_MONGOLAB_URI);
+
+var Schema = mongoose.Schema;
+
+var testSchema = {
+  demo: String,
+};
+
+var Test = mongoose.model('Test', testSchema);
+
+Test.find({}, function(err, test) {
+});
+
+var a = new Test({demo: "asdf"});
+a.save();
